3b1ddff2363dc5fa2d10c6e06d27ee08f496a992
Javi <javi@Javis-Mac.local>
2018-02-11 13:23:49 -0800

    Initial Commit

---
+++ PruebaGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaGit
+//
+//  Created by Javi on 11/02/2018.
+//  Copyright © 2018 Javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaGit
+//
+//  Created by Javi on 11/02/2018.
+//  Copyright © 2018 Javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
